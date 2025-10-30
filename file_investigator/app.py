2050e00021ab34ff57162539e370d3a4fadc8c3b
Mike Moore <mick.moore@hotmail.com>
2025-10-30 12:12:49 +0000

    Update app to use dropdowns, update requirements

--- file_investigator/app.py
+++ file_investigator/app.py
@@ -1,35 +1,52 @@
+import httpx
+import ipaddress
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
 from flask import Flask, request, render_template
 from markupsafe import escape
-from datetime import datetime
-import ipaddress
-import httpx
-import os
 
+load_dotenv()
 
-abuseipdb_api_key = os.environ.get("ABUSEIPDB_API_KEY")
-virustotal_api_key = os.environ.get("VIRUSTOTAL_API_KEY")
+abuseipdb_api_key = os.getenv("ABUSEIPDB_API_KEY")
+virustotal_api_key = os.getenv("VIRUSTOTAL_API_KEY")
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<p>Welcome To the Index Page</p>"
+    return "index"
 
 
-@app.route("/check-ip-abuseipdb")
-def check_ip_abuseipdb():
-    ip = request.args.get("ip")
+@app.route("/check-ip", methods=["GET", "POST"])
+def check_ip():
+    if request.method == "GET":
+        source = None
+        results = None
+    else:
+        ip = request.form.get("ip")
+        source = request.form.get("source")
 
-    if not ip:
-        return "<p> No IP address provided"
+        if not ip:
+            return "<p> No IP address provided </p>"
 
-    try:
-        ipaddress.ip_address(ip)
-    except ValueError:
-        return "<p> Invalid IP address provided </p>"
+        try:
+            ip = str(ipaddress.ip_address(ip))
+        except ValueError:
+            return "<p> Invalid IP address provided </p>"
 
+        if source == "abuseipdb":
+            results = check_ip_abuseipdb(ip)
+        elif source == "virustotal":
+            results = check_ip_virustotal(ip)
+        else:
+            return "<p> Invalid source provided </p>"
 
+    return render_template("check_ip.html", results=results, source=source)
+
+def check_ip_abuseipdb(ip: str):
     url = "https://api.abuseipdb.com/api/v2/check"
     params = {"ipAddress": ip}
     headers = {"Accept": "application/json", "Key": abuseipdb_api_key}
@@ -42,7 +59,7 @@ def check_ip_abuseipdb():
     response_data = response.json()["data"]
 
     results = {
-        "ip": ip,
+        "ip": response_data.get("ipAddress"),
         "country_code": response_data.get("countryCode"),
         "usage_type": response_data.get("usageType"),
         "isp": response_data.get("isp"),
@@ -53,21 +70,10 @@ def check_ip_abuseipdb():
         "retrieved_at": datetime.now().isoformat(),
     }
 
-    return render_template("check_ip_abuseipdb.html", results=results)
+    return results
 
 
-@app.route("/check-ip-virustotal")
-def check_ip_virustotal():
-    ip = request.args.get("ip")
-
-    if not ip:
-        return "<p> No IP address provided"
-
-    try:
-        ipaddress.ip_address(ip)
-    except ValueError:
-        return "<p> Invalid IP address provided </p>"
-
+def check_ip_virustotal(ip: str):
     url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
     headers = {
         "Accept": "application/json",
@@ -97,7 +103,7 @@ def check_ip_virustotal():
     whois_abuse_mailbox = whois_dict.get("abuse-mailbox")
 
     results = {
-        "ip": ip,
+        "ip": response_data.get("id"),
         "country": response_data_attributes.get("country"),
         "continent": response_data_attributes.get("continent"),
         "asn": response_data_attributes.get("asn"),
@@ -116,4 +122,4 @@ def check_ip_virustotal():
         "retrieved_at": datetime.now().isoformat(),
     }
 
-    return render_template("check_ip_virustotal.html", results=results)
+    return results
